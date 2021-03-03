5b0cb03743308a520e50c8fb074c3862ad1333ca
xiayh17 <xiayh17@gmail.com>
2021-03-03 15:35:35 +0800

    ☕️add qc_heatcor_server

--- R/app_server.R
+++ R/app_server.R
@@ -9,4 +9,5 @@ app_server <- function( input, output, session ) {
   mod_upload_server("upload_ui_1")
   mod_qc_pca_server("qc_pca_ui_1")
   mod_qc_heat1000_server("qc_heat1000_ui_1")
+  mod_qc_heatcor_server("qc_heatcor_ui_1")
 }
