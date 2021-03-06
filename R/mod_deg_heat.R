9e34ccecf853712ea0d585c6dc80e0b5cab326c0
xiayh17 <xiayh17@gmail.com>
2021-03-06 23:07:03 +0800

    nothing

--- R/mod_deg_heat.R
+++ R/mod_deg_heat.R
@@ -4,28 +4,28 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_deg_heat_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+
   )
 }
-    
+
 #' deg_heat Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_deg_heat_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_deg_heat_ui("deg_heat_ui_1")
-    
+
 ## To be copied in the server
 # mod_deg_heat_server("deg_heat_ui_1")
